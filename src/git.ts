e863e8b9bac2b47a8d29a5b481ef11b58bfb675f
Yarden Shoham <git@yardenshoham.com>
2024-03-28 11:38:12 +0200

    Fix error log (#123)

--- src/git.ts
+++ src/git.ts
@@ -5,6 +5,8 @@ export const getPrBranchName = (
   giteaMajorMinorVersion: string,
 ) => `backport-${prNumber}-v${giteaMajorMinorVersion}`;
 
+const decoder = new TextDecoder();
+
 export const initializeGitRepo = async (user: string, email: string | null) => {
   await cmd.run("git", {
     args: [
@@ -76,7 +78,11 @@ export const cherryPickPr = async (
   });
 
   if (!success) {
-    console.error(`Cherry-pick failed:\n${stdout}\n${stderr}`.trim());
+    console.error(
+      `Cherry-pick failed:\n${decoder.decode(stdout)}\n${
+        decoder.decode(stderr)
+      }`.trim(),
+    );
     await cmd.run("git", {
       cwd: "gitea",
       args: ["cherry-pick", "--abort"],
