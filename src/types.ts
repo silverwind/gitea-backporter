602b1c1ee5c2ed2802d10f2b87c2078285b97215
silverwind <me@silverwind.io>
2024-07-13 17:20:54 +0200

    fmt

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,13 @@
-import { Endpoints } from 'npm:@octokit/types@13.5.0';
+import { Endpoints } from "npm:@octokit/types@13.5.0";
 
-type IssueGetResponse = Endpoints["GET /repos/{owner}/{repo}/issues/{issue_number}"]["response"]["data"];
-type PullGetResponse = Endpoints["GET /repos/{owner}/{repo}/pulls/{pull_number}"]["response"]["data"];
+type IssueGetResponse =
+  Endpoints["GET /repos/{owner}/{repo}/issues/{issue_number}"]["response"][
+    "data"
+  ];
+type PullGetResponse =
+  Endpoints["GET /repos/{owner}/{repo}/pulls/{pull_number}"]["response"][
+    "data"
+  ];
 
-export type Issue = IssueGetResponse
-export type PullRequest = PullGetResponse
+export type Issue = IssueGetResponse;
+export type PullRequest = PullGetResponse;
